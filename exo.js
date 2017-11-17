7824dcef7e8f20b9d8ba13349113356c5afbf78f
JanMaier <31658349+JanMaier@users.noreply.github.com>
2017-11-17 15:31:07 +0100

    Exo

---
+++ exo.js
@@ -0,0 +1,22 @@
+var eleves = [
+	{
+		nom: "Maier",
+		prenom: "Jan",
+		age: 25,
+		taille: 1.88,
+		cheveux: 'blonds',
+		myope: true,
+		lieu: "Paris",
+		loisirs: ["musique", "procrastination"]
+	},
+	{
+		nom: "Berger",
+		prenom: "Kenneth",
+		age: 26,
+		taille: 1.91,
+		cheveux: 'marrons',
+		myope: false,
+		lieu: "Lille",
+		loisirs: ["lecture", "piano"]
+	}
+]
